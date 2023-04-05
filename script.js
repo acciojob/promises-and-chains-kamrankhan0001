0f3dd0a62a276c3c35902d69ea094a8fca327acc
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-06 02:03:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+var ageElement= documents.querySelector("#age");
+var nameElement= documents.querySelector("#name");
+var btnElement= documents.querySelector("#btn");
+
+var promise = new Promise(resolve,reject)=>{
+	setTimeout(()=>{
+		var age=ageElement.value;
+		var name=nameElement.value;
+		if(!(age && name)){
+			alert("please enter a valid details");
+		}
+		return;
+		 const promise = new Promise((resolve, reject) => {
+      if (ageInput.value >= 18) {
+        setTimeout(() => {
+          resolve(`Welcome, ${nameInput.value}. You can vote.`);
+        }, 4000);
+      } else {
+        reject(`Oh sorry ${nameInput.value}. You aren't old enough.`);
+      }
+    });
+
+    promise
+      .then((message) => alert(message))
+      .catch((error) => alert(error));
+  });
+	
+}
