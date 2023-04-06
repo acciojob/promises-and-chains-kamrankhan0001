bcb1ba0ff0b76f501a14fc90062ac4c9fcbe1b9a
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-06 11:07:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,58 +1,59 @@
 //your JS code here. If required.
-// var ageElement= documents.querySelector("#age");
-// var nameElement= documents.querySelector("#name");
-// var btnElement= documents.querySelector("#btn");
+var ageElement= documents.querySelector("#age");
+var nameElement= documents.querySelector("#name");
+var btnElement= documents.querySelector("#btn");
 
-
-// 		 const promise = new Promise((resolve, reject) => {
+if(!(age && name)){
+		alert("please enter a valid details");
+// 		
+		return;
+}
+		 const promise = new Promise((resolve, reject) => {
       
-//         setTimeout(() => {
-// 			var age=ageElement.value;
-// 		var name=nameElement.value;
-// 		if(!(age && name)){
-// 			alert("please enter a valid details");
-// 		}
-// 			return;
-// 			if(age>18){
-//           resolve(`Welcome, ${name}. You can vote.`);
-//         }, 4000);
-//       } else {
-//         reject(`Oh sorry ${name}. You aren't old enough.`);
-//       }
-//     });
+        setTimeout(() => {
+			var age=ageElement.value;
+		var name=nameElement.value;
+// 		
+			if(age>18){
+          resolve(`Welcome, ${name}. You can vote.`);
+        }, 4000);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    });
 
-//     function callPromise(){
-// 		return promise;
-// 	}
+    function callPromise(){
+		return promise;
+	}
 	
 // }
 
 
 
-  const form = document.querySelector('form');
-  const ageInput = document.querySelector('#age');
-  const nameInput = document.querySelector('#name');
-  const btn = document.querySelector('#btn');
+  // const form = document.querySelector('form');
+  // const ageInput = document.querySelector('#age');
+  // const nameInput = document.querySelector('#name');
+  // const btn = document.querySelector('#btn');
 
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
+  // form.addEventListener('submit', (event) => {
+  //   event.preventDefault();
 
-    if (!ageInput.value || !nameInput.value) {
-      alert('Please enter valid details');
-      return;
-    }
+  //   if (!ageInput.value || !nameInput.value) {
+  //     alert('Please enter valid details');
+  //     return;
+  //   }
 
-    const promise = new Promise((resolve, reject) => {
-      if (ageInput.value >= 18) {
-        setTimeout(() => {
-          resolve(`Welcome, ${nameInput.value}. You can vote.`);
-        }, 4000);
-      } else {
-        reject(`Oh sorry ${nameInput.value}. You aren't old enough.`);
-      }
-    });
+  //   const promise = new Promise((resolve, reject) => {
+    //   if (ageInput.value >= 18) {
+    //     setTimeout(() => {
+    //       resolve(`Welcome, ${nameInput.value}. You can vote.`);
+    //     }, 4000);
+    //   } else {
+    //     reject(`Oh sorry ${nameInput.value}. You aren't old enough.`);
+    //   }
+    // });
 
-    promise
-      .then((message) => alert(message))
-      .catch((error) => alert(error));
+    // promise
+    //   .then((message) => alert(message))
+    //   .catch((error) => alert(error));
   });
